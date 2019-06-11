b2d1f7debc8cdffeaaca44c091e99a3247561a9e
Brad Westfall <brad@azpixels.com>
2019-06-11 10:01:28 -0700

    starting point

--- src/ui/ProjectSidebar.js
+++ src/ui/ProjectSidebar.js
@@ -1,34 +1,22 @@
 import React from 'react'
-import { NavLink, withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
 const ProjectSidebar = ({ match }) => (
   <aside className="project-sidebar">
     <nav className="overview">
-      <NavLink activeClassName="active" to={`${match.url}/overview`} className="heading-4">
+      <Link to={`${match.url}/overview`} className="heading-4">
         Project Overview
-      </NavLink>
+      </Link>
     </nav>
     <nav className="open spacing">
       <h1 className="heading-4">Develop</h1>
       <div className="spacing-medium">
-        <NavLink activeClassName="active" to={`${match.url}/authentication`}>
-          Authentication
-        </NavLink>
-        <NavLink activeClassName="active" to={`${match.url}/database`}>
-          Database
-        </NavLink>
-        <NavLink activeClassName="active" to={`${match.url}/storage`}>
-          Storage
-        </NavLink>
-        <NavLink activeClassName="active" to={`${match.url}/hosting`}>
-          Hosting
-        </NavLink>
-        <NavLink activeClassName="active" to={`${match.url}/functions`}>
-          Functions
-        </NavLink>
-        <NavLink activeClassName="active" to={`${match.url}/ml-kit`}>
-          ML Kit
-        </NavLink>
+        <Link to={`${match.url}/authentication`}>Authentication</Link>
+        <Link to={`${match.url}/database`}>Database</Link>
+        <Link to={`${match.url}/storage`}>Storage</Link>
+        <Link to={`${match.url}/hosting`}>Hosting</Link>
+        <Link to={`${match.url}/functions`}>Functions</Link>
+        <Link to={`${match.url}/ml-kit`}>ML Kit</Link>
       </div>
     </nav>
     <nav className="spacing-small">
