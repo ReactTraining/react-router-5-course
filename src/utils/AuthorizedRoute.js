093740e123d9bfa1e2299061d0b2299fd6890c17
Brad Westfall <brad@azpixels.com>
2019-06-19 13:18:07 -0700

    starting point for video

---
+++ src/utils/AuthorizedRoute.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useAuthUser } from './AuthUser'
+
+const AuthorizedRoute = ({ component, history, ...rest }) => {
+  const { logged } = useAuthUser()
+
+  if (logged === null) return <div>Loading...</div>
+  if (logged !== true) return <Redirect push to="/auth" />
+  return <Route component={component} {...rest} />
+}
+
+export default AuthorizedRoute
