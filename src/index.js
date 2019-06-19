d988c01f3b56e0c82004c25fc7b27f71e6a32a64
Brad Westfall <brad@azpixels.com>
2019-06-19 15:19:59 -0700

    finished video

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+function Contact() {
+  return <h1>Contact Page</h1>
+}
+
+function Home() {
+  return <h1>Home Page</h1>
+}
 
 const App = () => (
-  <div>
-    <h1>Welcome!</h1>
-  </div>
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={Home} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </BrowserRouter>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
