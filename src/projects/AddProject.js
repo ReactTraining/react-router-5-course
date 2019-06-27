acb3680a37648a860cee01ffc4df1f68b52fc92e
Brad Westfall <brad@azpixels.com>
2019-06-27 13:48:04 -0700

    prime for video

--- src/projects/AddProject.js
+++ src/projects/AddProject.js
@@ -1,19 +1,15 @@
-import React, { useState } from 'react'
-import { Prompt } from 'react-router-dom'
+import React from 'react'
 import Card from '../ui/Card'
 
 const AddProject = () => {
-  const [formIsDirty, setFormIsDirty] = useState(false)
-
   function handleSubmit(e) {
     e.preventDefault()
   }
 
   return (
     <Card style={{ minHeight: '15em' }}>
-      <Prompt when={formIsDirty} message="Are you sure you want to leave this form before saving?" />
       <form className="spacing" onSubmit={handleSubmit}>
-        <input onChange={() => setFormIsDirty(true)} type="text" placeholder="Project Name" required />
+        <input type="text" placeholder="Project Name" required />
         <button type="submit" className="button">
           Add Project
         </button>
