4cca89127d8b97a89e1b11c50756e2d7caa69cce
Brad Westfall <brad@azpixels.com>
2019-07-01 13:10:32 -0700

    notes

--- src/projects/authentication/AuthenticationLayout.js
+++ src/projects/authentication/AuthenticationLayout.js
@@ -11,7 +11,19 @@ import Templates from './Templates'
 const AuthenticationLayout = ({ match, location }) => {
   return (
     <Fragment>
+      {/*
+        In the video, I somewhat glossed over why these are only the same value before
+        the redirect and then they are different. The reason is because `match.url` only
+        has what the URL value is up to the route that matched this component. So in
+        other words, the route that matched for `AuthenticationLayout` is
+        `/projects/react-training/authentication`. The `match.url` here will only be
+        that value even if the patch is much longer. The `location.pathname` is the
+        true representation of the full path in the browser. Therefore, if they are the
+        same, that indicates we are at `/projects/react-training/authentication` and
+        need to do a redirect.
+      */}
       {match.url === location.pathname && <Redirect to={`${match.url}/users`} />}
+
       <PageHeader title="Authentication" useMaxWidth={false}>
         <PageHeaderTabs>
           <Tab to={`${match.url}/users`}>Users</Tab>
@@ -21,7 +33,7 @@ const AuthenticationLayout = ({ match, location }) => {
       </PageHeader>
       <Panel>
         <TransitionGroup className="animated-cards">
-          <CSSTransition key={location.key} timeout={4000} classNames="animated-card">
+          <CSSTransition key={location.key} timeout={600} classNames="animated-card">
             <Switch location={location}>
               <Route path={`${match.path}/users`} component={Users} />
               <Route path={`${match.path}/signin-method`} component={SigninMethods} />
