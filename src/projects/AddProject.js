dcdaecc819a1fb6d612956979f591c0e6235b691
Brad Westfall <brad@azpixels.com>
2019-07-12 13:01:59 -0700

    finished vide

--- src/projects/AddProject.js
+++ src/projects/AddProject.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, Prompt } from 'react-router-dom'
 import Card from '../ui/Card'
 
 const AddProject = ({ location }) => {
+  const [isDirty, setIsDirty] = useState(false)
+
   function handleSubmit(e) {
     e.preventDefault()
   }
 
   return (
     <Card style={{ minHeight: '15em' }}>
+      <Prompt when={isDirty} message="Are you sure you want to leave an unsaved form?" />
       <form className="spacing" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Project Name" required />
+        <input onChange={() => setIsDirty(true)} type="text" placeholder="Project Name" required />
         <footer className="horizontal-spacing">
           <button type="submit" className="button">
             Add Project
